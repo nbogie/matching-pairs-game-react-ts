4d27cda02093e8e2974359642778b66e8f1829ab
nbogie <neillbogie@googlemail.com>
2023-07-18 09:38:43 +0100

    formatting only

--- src/components/PairsGame.tsx
+++ src/components/PairsGame.tsx
@@ -1,44 +1,46 @@
-import { useMemo, useReducer } from 'react';
+import { useMemo, useReducer } from "react";
 import { Card } from "../gameCore/card";
-import { GameState } from '../gameCore/gameState';
-import { reducerFn } from '../reducer/reducer';
-import { CardView } from './CardView';
-import { makeEmojisDeck } from './Deck';
+import { GameState } from "../gameCore/gameState";
+import { reducerFn } from "../reducer/reducer";
+import { CardView } from "./CardView";
+import { makeEmojisDeck } from "./Deck";
 
 export default function PairsGame() {
-
     const initialState: GameState = useMemo(() => {
         return {
             clickCount: 0,
             turnStatus: {
-                title: 'noneTurned'
+                title: "noneTurned",
             },
             deck: makeEmojisDeck(),
-            leaderboard: []
-        }
+            leaderboard: [],
+        };
     }, []);
 
     const [gameState, dispatch] = useReducer(reducerFn, initialState);
     function handleClickOnMat() {
-        if (gameState.turnStatus.title === 'twoTurned') {
-            dispatch({ type: 'clickAcknowledge' })
+        if (gameState.turnStatus.title === "twoTurned") {
+            dispatch({ type: "clickAcknowledge" });
         }
     }
 
     function handleClickCard(c: Card) {
-        if (gameState.turnStatus.title === 'twoTurned') {
-            dispatch({ type: 'clickAcknowledge' })
+        if (gameState.turnStatus.title === "twoTurned") {
+            dispatch({ type: "clickAcknowledge" });
         } else {
-            dispatch({ type: 'flipCard', card: c })
+            dispatch({ type: "flipCard", card: c });
         }
     }
-
 
     return (
         <div className="mat" onClick={handleClickOnMat}>
             <div className="cardset">
                 {gameState.deck.map((c: Card, ix: number) => (
-                    <CardView card={c} key={ix} handleClickCard={handleClickCard} />
+                    <CardView
+                        card={c}
+                        key={ix}
+                        handleClickCard={handleClickCard}
+                    />
                 ))}
             </div>
             <div>TurnStatus: {gameState.turnStatus.title}</div>
